c9319e6d4b887b9aef2553971b6f40aa6e228cdf
Alex Wilson <alexwilsonlearn@gmail.com>
2020-11-12 07:12:27 -0600

    Adding playRound function to define potention selections and outcomes for each round

--- scripts.js
+++ scripts.js
@@ -10,3 +10,30 @@ function computerPlay(cpuPick){
         return 'scissors';
     }
    }
+
+   //Defining potential Selections and Outcomes
+   function playRound(playerSelection, computerSelection){
+
+      
+        if (playerSelection === 'rock' && computerSelection ==='paper'){
+        return 'You lose. Paper beats Rock'
+        }else if (playerSelection === 'rock' && computerSelection ==='scissors'){
+        return 'You win. Rock beats scissors';
+        }else if (playerSelection === 'rock' && computerSelection === 'rock'){
+        return 'Tie. You both picked Rock.'
+        }else if (playerSelection === 'paper' && computerSelection === 'rock'){
+        return 'You win. Paper beats Rock.';
+        }else if (playerSelection === 'paper' && computerSelection === 'paper'){
+        return 'Tie. You both picked Paper.';
+        }else if (playerSelection === 'paper' && computerSelection === 'scissors'){
+        return 'You lose. Scissors beats Paper.';
+        }else if (playerSelection === 'scissors' && computerSelection === 'rock'){
+        return 'You lose. Rock beats Scissors.';
+        }else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return 'You win. Scissors beats Paper.';
+        }else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
+        return 'Tie. You both picked Scissors.';
+        }else{
+        return 'Something went wrong. Please report the problem and refresh';
+        }
+    }
